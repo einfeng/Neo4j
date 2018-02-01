b340f2355096358cd6fc558a18a20481ab190516
einfeng <36031859+einfeng@users.noreply.github.com>
2018-02-01 17:26:24 +0800

    2018/02/01
    2018/02/01

--- src/main/java/com/ace/Neo4j/controller/MainController.java
+++ src/main/java/com/ace/Neo4j/controller/MainController.java
@@ -10,7 +10,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
